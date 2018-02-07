7c4f112616075406a5836ec772ff02051a7cb9e2
Oliver Kraemer <oliverkra@gmail.com>
2018-02-07 05:57:13 -0200

    Criação de um simples teste para garantir que as chamadas HTTP estejam sendo executadas corretamente

---
+++ tests/Net/Http/ClientTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AgendarEntregaTest\Net\Http;
+
+use PHPUnit\Framework\TestCase;
+use AgendarEntrega\Net\Http\Client;
+
+class ClientTest extends TestCase {
+    private $client;
+
+    protected function setUp() {
+        $this->client = new Client();
+    }
+
+    public function testGetShouldReturnWithSuccess() {
+        $res = $this->client->get('https://jsonplaceholder.typicode.com/posts/1');
+        $this->assertEquals(200, $res->getStatusCode());
+        $body = $res->asJson();
+        $this->assertEquals(1, $body['id']);
+    }
+
+    public function testPostShouldSendWithSuccess() {
+        $res = $this->client->post('https://jsonplaceholder.typicode.com/posts', [
+            'userId' => 1,
+            'title' => 'foo',
+            'body' => 'bar',
+        ]);
+        $this->assertEquals(201, $res->getStatusCode());
+        $body = $res->asJson();
+        $this->assertEquals(101, $body['id']);
+    }
+}
