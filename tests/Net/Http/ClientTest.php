6674d567fe714b2960c19e1dd13baa76ccc04098
Oliver Kraemer <oliverkra@gmail.com>
2018-02-07 07:55:07 -0200

    Utilização de fakeserver nos testes

--- tests/Net/Http/ClientTest.php
+++ tests/Net/Http/ClientTest.php
@@ -13,20 +13,20 @@ class ClientTest extends TestCase {
     }
 
     public function testGetShouldReturnWithSuccess() {
-        $res = $this->client->get('https://jsonplaceholder.typicode.com/posts/1');
+        $res = $this->client->get('http://fakeserver:3000/posts/1');
         $this->assertEquals(200, $res->getStatusCode());
         $body = $res->asJson();
         $this->assertEquals(1, $body['id']);
     }
 
     public function testPostShouldSendWithSuccess() {
-        $res = $this->client->post('https://jsonplaceholder.typicode.com/posts', array(
+        $res = $this->client->post('http://fakeserver:3000/posts', array(
             'userId' => 1,
             'title' => 'foo',
             'body' => 'bar',
         ));
         $this->assertEquals(201, $res->getStatusCode());
         $body = $res->asJson();
-        $this->assertEquals(101, $body['id']);
+        $this->assertGreaterThan(0, $body['id']);
     }
 }
